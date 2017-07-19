967650dc2e8b4e25dc164d7c242aacf189567eaa
Juan David Gomez <judagoes@gmail.com>
2017-07-19 13:54:36 -0500

    Plantilla Registro

---
+++ src/app/layout/layout-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LayoutComponent } from './layout.component';
+
+const routes: Routes = [
+    {
+        path: '', component: LayoutComponent,
+        children: [
+            //{ path: 'newer-cmp', loadChildren: './newer-cmp/newer-cmp.module#NewerCmpModule' },
+        ]
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class LayoutRoutingModule { }
